be6e62c18f21dc21b8d4a00bb22134633c239f63
Lucas Finatti <lucas.finatti@hotmail.com>
2024-07-10 13:11:15 -0300

    [INIT]

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+    const root = await prisma.usuario.upsert({
+        where: { login: 'x506640' },
+        create: {
+            login: 'x506640',
+            nome: 'Lucas Finatti',
+            email: 'lfinatti@prefeitura.sp.gov.br',
+            status: 1,
+            permissao: 'DEV'
+        },
+        update: {
+            login: 'x506640',
+            nome: 'Lucas Finatti',
+            email: 'lfinatti@prefeitura.sp.gov.br',
+            status: 1,
+            permissao: 'DEV'
+        }
+    });
+    console.log(root);
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
